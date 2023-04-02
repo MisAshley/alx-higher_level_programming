b021e13b10c0da3566b774d28ea4151fe734d9f1
MisAshley <wise60030@gmail.com>
2023-04-02 12:21:49 -0700

    Task One

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-'''1-square.py: Python script that defines square,
-   private instantiation attribute of size'''
+"""Define a class Square."""
 
 
 class Square:
-        '''Creates Square type'''
+    """Represent a square."""
 
-        def __init__(self, size):
-            '''Initializes Square with size'''
-
-            self.__size = size
+    def __init__(self, size):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size 
